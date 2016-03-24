40fab508b03aa58a536ca4bedfd0383504488e12
Martin Jay <martinjay180@gmail.com>
2016-03-24 16:30:01 -0400

    updates node packages.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var player = require('play-sound')(opts = {})
+const fs = require('fs');
+var sfx = require("sfx");
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 require('general-js');
+
+var sounds = fs.readdirSync("sounds");
 
 app.get('/', function(req, res){
 //    res.send('<h1>Hello world</h1>');
@@ -13,10 +16,8 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.emit('data', socket.id);
     
-  socket.on('play sound', function(msg){
-    player.play('media/' + msg, function(err){
-        console.log(err);
-    })  
+  socket.on('play sound', function(msg){  
+    sfx.play("ping");
     io.emit('play sound', {id: msg, userId: socket.id});
   });
     
