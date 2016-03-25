208f75b9b1150029a93cd1055065557d00e06367
Martin Jay <martinjay180@gmail.com>
2016-03-25 12:50:59 -0400

    adds callback for sound finished.

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 const fs = require('fs');
 var sfx = require("sfx");
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 require('general-js');
+var _ = require('underscore');
 
 var sounds = fs.readdirSync("sounds");
+sounds = _.map(sounds, function (sound, index) {
+    if (!sound.startsWith(".")) {
+        return {
+            id: index,
+            path: sound,
+            playing: false
+        };
+    }
+});
+sounds = _.omit(sounds, _.isUndefined)
 
-app.get('/', function(req, res){
-//    res.send('<h1>Hello world</h1>');
-    res.sendFile(__dirname + '/client.html');
-    //player.play('test.aiff', function(err){})    
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client.html');   
 });
 
-io.on('connection', function(socket){
-  socket.emit('data', socket.id);
-    
-  socket.on('play sound', function(msg){  
-    sfx.play("ping");
-    io.emit('play sound', {id: msg, userId: socket.id});
-  });
-    
+io.on('connection', function (socket) {
+    socket.emit('data', socket.id);
+    socket.emit('sounds', sounds);
+
+    socket.on('play sound', function (soundId) {
+        var sound = _.findWhere(sounds, {
+            id: soundId
+        });
+        var path = "./{0}/{1}".format("sounds", sound.path);
+        if (sound.playing) {
+            sfx.stop(path);
+        } else {
+            sfx.play(path, function(a, b, c){
+                soundFinished(socket, sound);
+            });
+        }
+        sound.playing = !sound.playing;
+        socket.emit('sounds', sounds);
+    });
+
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+function soundFinished(socket, sound){
+    sound.playing = false;
+    socket.emit('sounds', sounds);
+}
 
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
